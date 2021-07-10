94b418c6574ce8e4bbccb674e48be0624c02771c
Ayush <Ayush-15@live.com>
2021-07-10 11:47:17 +0530

    .

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,11 +2,11 @@ import ThemeSwitch from './ThemeSwitcher'
 
 const Intro = () => {
   return (
-    <section className="flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="justify-between flex-row flex items-center mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      <div className="text-center md:text-left text-lg mt-5 md:pl-8">
+      <div className="md:pr-8">
         <ThemeSwitch />
       </div>
     </section>
